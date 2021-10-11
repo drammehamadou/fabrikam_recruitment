3c6b98bf5631c9a904d8c54977edf316e6ef8d8d
drammehamadou <aadrammeh@hotmail.co.uk>
2021-10-11 13:10:30 +0100

    Create env

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 
+//environment variables
+require('dotenv/config');
+
+const api = process.env.API_URL;
+
 app.get('/', (req, res) => {
-    res.send(' Hello amadou');
+    res.send(' Hello Amadou');
 })
 
 app.listen(2021, () => {
+    console.log(api);
     console.log('Server is running http://localhost:2021');
 })

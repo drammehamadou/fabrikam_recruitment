8dc94b95b959b6920638114c265d3a5e7cfdac52
drammehamadou <aadrammeh@hotmail.co.uk>
2021-10-11 13:00:57 +0100

    Created the serve with express

--- server/index.js
+++ server/index.js
@@ -1,1 +1,10 @@
-console.log("Hello Amadou")
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(' Hello amadou');
+})
+
+app.listen(2021, () => {
+    console.log('Server is running http://localhost:2021');
+})

219234ff8029931cf7d28c345d43affc6e0492f1
drammehamadou <aadrammeh@hotmail.co.uk>
2021-10-12 09:53:58 +0100

    Create Training model and Schema

--- server/models/booking.js
+++ server/models/booking.js
@@ -1,9 +1,46 @@
 const mongoose = require('mongoose');
 
 const bookingSchema = mongoose.Schema({
-    title:  String, 
-    description: String,
-    image: String
+    title:  {
+        type: String,
+        required: true 
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        default: ''
+    },
+    images: [{
+            type: String
+        }],
+    price: {
+        type: Number,
+        default: 0
+    },
+    subject: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'subject',
+        required: true
+    },
+    rating: {
+        type: Number,
+        default: 0
+    },
+    numReviews: {
+        type: Number,
+        default: 0
+    },
+    isFeatured: {
+        type: Boolean,
+        default: false
+    },
+    dateCreated: {
+        type: Date,
+        default: Date.now
+    }
 })
 
 exports.Booking = mongoose.model('Booking', bookingSchema);

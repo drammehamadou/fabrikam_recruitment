ef5a58fcc38930fc3f964982acddb6613c08878a
drammehamadou <aadrammeh@hotmail.co.uk>
2021-10-12 09:59:13 +0100

    Create subject model & Schema

--- server/models/subject.js
+++ server/models/subject.js
@@ -1,7 +1,16 @@
 const mongoose = require('mongoose');
 
 const subjectSchema = mongoose.Schema({
- 
+    name: {
+        type: String,
+        required: true
+    },
+    icon: {
+        type: String
+    },
+    color: {
+        type: String
+    }
 })
 
 exports.Subject = mongoose.model('Subject', subjectSchema);
--- server/models/training.js
+++ server/models/training.js
@@ -1,7 +1,46 @@
 const mongoose = require('mongoose');
 
 const trainingSchema = mongoose.Schema({
-    
+    title:  {
+        type: String,
+        required: true 
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        default: ''
+    },
+    images: [{
+            type: String
+        }],
+    price: {
+        type: Number,
+        default: 0
+    },
+    subject: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'subject',
+        required: true
+    },
+    rating: {
+        type: Number,
+        default: 0
+    },
+    numReviews: {
+        type: Number,
+        default: 0
+    },
+    isFeatured: {
+        type: Boolean,
+        default: false
+    },
+    dateCreated: {
+        type: Date,
+        default: Date.now
+    }
 })
 
 exports.Training = mongoose.model('Training', trainingSchema);

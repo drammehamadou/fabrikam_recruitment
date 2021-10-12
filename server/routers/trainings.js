8e9be880a8d06b432966f460e37f49e69ac08014
drammehamadou <aadrammeh@hotmail.co.uk>
2021-10-12 13:47:31 +0100

    Create update, delete training REST API and validate id

--- server/routers/trainings.js
+++ server/routers/trainings.js
@@ -49,4 +49,49 @@ router.get(`/:id`, async (req, res) => {
     res.send(training);
 })
 
+//update a training
+router.put(`/:id`, async (req, res) => {
+    //validate
+    if(!mongoose.isValidObjectId(req.params.id)) {
+        res.status(400).send('Invalid Training Id')
+    }
+    const course = await Course.findById(req.body.course);
+    if(!course) return res.status(400).send('Invalid Course')
+    const training = await Training.findByIdAndUpdate(
+        req.params.id, 
+        {
+            title: req.body.title,
+            description: req.body.description,
+            image: req.body.image,
+            price: req.body.price,
+            course: req.body.course,
+            rating: req.body.rating,
+            reviews: req.body.reviews,
+            isFeatured: req.body.isFeatured
+        },
+        //return new updated data
+        {new: true}
+    )
+    if (!training)
+return res.status(404).send('The training cannot be updated.')
+
+res.send(training);
+})
+
+//delete a training
+router.delete(`/:id`, (req, res) => {
+    Training.findByIdAndRemove(req.params.id).then(training => {
+        if(training) {
+            return res.status(200).json({
+                success: true, message: 'The training is deleted.'
+            })
+        } else {
+            return res.status(404).json({
+                success: false, message: 'training not found'})
+        }
+    }).catch(err => {
+        return res.status(500).json({success: false, error: err})
+    })
+})
+
 module.exports=router;

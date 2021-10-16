9c0def4585d1e4ff94ae484a053d5aa1a9714bed
drammehamadou <aadrammeh@hotmail.co.uk>
2021-10-16 19:21:12 +0100

    Create components

--- nx-library/apps/fabrikam/src/app/app.module.ts
+++ nx-library/apps/fabrikam/src/app/app.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { TrainingListComponent } from './components/training-list/training-list.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LandingComponent, TrainingListComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ nx-library/apps/fabrikam/src/app/components/training-list/training-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nx-library-training-list',
+  templateUrl: './training-list.component.html',
+  styleUrls: ['./training-list.component.scss']
+})
+export class TrainingListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

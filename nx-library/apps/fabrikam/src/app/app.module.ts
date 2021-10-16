5fb7eb70773f19357f89d80418da867393eafe8c
drammehamadou <aadrammeh@hotmail.co.uk>
2021-10-16 19:34:41 +0100

    Create routes

--- nx-library/apps/fabrikam/src/app/app.module.ts
+++ nx-library/apps/fabrikam/src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LandingComponent } from './components/landing/landing.component';
 import { TrainingListComponent } from './components/training-list/training-list.component';
 
+const routes: Routes = [
+  { path: '', component: LandingComponent },
+  { path: 'trainings', component: TrainingListComponent}
+]
+
 @NgModule({
   declarations: [AppComponent, LandingComponent, TrainingListComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
 })

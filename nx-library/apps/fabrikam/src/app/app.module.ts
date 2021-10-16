ec50f82ce2590c75fbdcee456dd653d7a34e727e
drammehamadou <aadrammeh@hotmail.co.uk>
2021-10-16 19:46:20 +0100

    Create header & footer

--- nx-library/apps/fabrikam/src/app/app.module.ts
+++ nx-library/apps/fabrikam/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './components/landing/landing.component';
 import { TrainingListComponent } from './components/training-list/training-list.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 const routes: Routes = [
   { path: '', component: LandingComponent },
@@ -12,7 +14,7 @@ const routes: Routes = [
 ]
 
 @NgModule({
-  declarations: [AppComponent, LandingComponent, TrainingListComponent],
+  declarations: [AppComponent, LandingComponent, TrainingListComponent, HeaderComponent, FooterComponent],
   imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],

7561df28deb329f9bc50d26c5db33235945426ed
drammehamadou <aadrammeh@hotmail.co.uk>
2021-10-20 10:58:14 +0100

    Use environment variables for API's - couses panel done

--- nx-library/libs/trainings/src/lib/services/courses.service.ts
+++ nx-library/libs/trainings/src/lib/services/courses.service.ts
@@ -2,33 +2,36 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Course } from '../models/course';
+import { environment }  from '@env/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
+  apiURLCourses = environment.apiUrl + 'courses';
 
   constructor(private http: HttpClient) { 
 
   }
 
   getCourse(courseId : string): Observable<Course> {
-    return this.http.get<Course>(`http://localhost:2021/api/v1/courses/${courseId}`)
+    return this.http.get<Course>(`${this.apiURLCourses}/${courseId}`)
   }
 
   getCourses(): Observable<Course[]> {
-    return this.http.get<Course[]>('http://localhost:2021/api/v1/courses/')
+    return this.http.get<Course[]>(this.apiURLCourses)
   }
 
   createCourse(course: Course): Observable<Course>  {
-    return this.http.post<Course>('http://localhost:2021/api/v1/courses/', course)
+    return this.http.post<Course>(this.apiURLCourses, course)
   }
 
   updateCourse(course: Course): Observable<Course>  {
-    return this.http.put<Course>('http://localhost:2021/api/v1/courses/' + course.id, course)
+    return this.http.put<Course>(
+      `${this.apiURLCourses}/${course.id}`, course)
   }
 
   deleteCourse(courseId: string): Observable<any> {
-    return this.http.delete<any>(`http://localhost:2021/api/v1/courses/${courseId}`)
+    return this.http.delete<any>(`${this.apiURLCourses}/${courseId}`)
   }
 }
